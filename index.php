dcd1d26dcca2eeaab03aab38e29be63eaa269ee1
Adian Tsosie <addve12@yahoo.com>
2019-01-14 11:53:46 -0700

    Demo webpage for first day

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset = "UTF-8"/>
-		<title>Basic Demo of Project Creaton</title>
+		<title>Basic Demo of Project Creation</title>
 	</head>
 	<body>
 		<h1>Deep Dive Fullstack Cohort 23</h1>
